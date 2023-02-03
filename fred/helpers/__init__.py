93acb91030794bc11ce7f4d9dedcfdc1d9c00f3b
fillthesavage <43685932+fillthesavage@users.noreply.github.com>
2023-02-03 13:09:17 -0800

    cleaning up leftover commits

--- fred/helpers/__init__.py
+++ fred/helpers/__init__.py
@@ -1,4 +1,5 @@
 from sys import version_info
+
 if version_info[0] >= 3:
     from urllib.parse import urlencode
     from urllib.request import Request, urlopen
@@ -13,6 +14,7 @@ from json import loads
 
 try:
     from pandas import DataFrame
+
     _has_pandas = True
 except ImportError:
     DataFrame = None
@@ -22,7 +24,8 @@ except ImportError:
 _USE_JOBLIB_CACHE = True
 _THROTTLE_REQUESTS = True
 
-def _fetch(url, ssl_verify = True):
+
+def _fetch(url, ssl_verify=True):
     """
     Helper funcation to fetch content from a given url.
     """
@@ -35,18 +38,20 @@ def _fetch(url, ssl_verify = True):
         ctx.verify_mode = ssl.CERT_NONE
 
         page = urlopen(req, context=ctx)
-    content = page.read().decode('utf-8')
+    content = page.read().decode("utf-8")
     page.close()
     return content
 
-def _url_builder(url_root,api_key,path,params):
+
+def _url_builder(url_root, api_key, path, params):
     """
     Helper funcation to build a parameterized url.
     """
-    params['api_key'] = api_key
+    params["api_key"] = api_key
     url_end = urlencode(params)
-    url = "%s%s%s" % (url_root,path,url_end)
+    url = "%s%s%s" % (url_root, path, url_end)
     return url
+
 
 def _convert(frame):
     """
@@ -55,8 +60,9 @@ def _convert(frame):
     frame = frame.convert_dtypes()
     for column in frame:
         if column in c.dates:
-            frame[column] = frame[column].astype('datetime64')
+            frame[column] = frame[column].astype("datetime64")
     return frame
+
 
 def _dict(content):
     """
@@ -64,12 +70,13 @@ def _dict(content):
     to a python dictionary for additional manipulation.
     """
     if _has_pandas:
-        data = _data_frame(content).to_dict(orient='records')
+        data = _data_frame(content).to_dict(orient="records")
     else:
         response = loads(content)
         key = [x for x in response.keys() if x in c.response_data][0]
         data = response[key]
     return data
+
 
 def _data_frame(content):
     """
@@ -82,6 +89,7 @@ def _data_frame(content):
     final_frame = _convert(frame)
     return final_frame
 
+
 def _csv(content):
     """
     Helper funcation that converts text-based get response
@@ -90,21 +98,24 @@ def _csv(content):
     response = _data_frame(content).to_csv(index=False)
     return response
 
+
 def _tab(content):
     """
     Helper funcation that converts text-based get response
     to tab separated values for additional manipulation.
     """
-    response = _data_frame(content).to_csv(index=False,sep='\t')
+    response = _data_frame(content).to_csv(index=False, sep="\t")
     return response
+
 
 def _pipe(content):
     """
     Helper funcation that converts text-based get response
     to pipe separated values for additional manipulation.
     """
-    response = _data_frame(content).to_csv(index=False,sep='|')
+    response = _data_frame(content).to_csv(index=False, sep="|")
     return response
+
 
 def _numpy(content):
     """
@@ -114,11 +125,13 @@ def _numpy(content):
     response = _data_frame(content).values
     return response
 
+
 def _json(content):
     """
     Pass response
     """
     return content
+
 
 def _xml(content):
     """
@@ -126,36 +139,55 @@ def _xml(content):
     """
     return content
 
+
 def _dispatch(response_type):
     if _has_pandas:
-        dispatch = {'dict': _dict,'json': _json,
-                    'xml': _xml,'df':_data_frame,
-                    'csv':_csv,'numpy':_numpy,
-                    'tab': _tab,'pipe': _pipe}
+        dispatch = {
+            "dict": _dict,
+            "json": _json,
+            "xml": _xml,
+            "df": _data_frame,
+            "csv": _csv,
+            "numpy": _numpy,
+            "tab": _tab,
+            "pipe": _pipe,
+        }
     else:
-        dispatch = {'dict': _dict,'json': _json,'xml': _xml, }
+        dispatch = {
+            "dict": _dict,
+            "json": _json,
+            "xml": _xml,
+        }
 
     return dispatch[response_type]
 
 
-def _get_request(url_root,api_key,path,response_type,params, ssl_verify):
+def _get_request(url_root, api_key, path, response_type, params, ssl_verify):
     """
     Helper funcation that requests a get response from FRED.
     """
-    url = _url_builder(url_root,api_key,path,params)
+    url = _url_builder(url_root, api_key, path, params)
     content = _fetch(url, ssl_verify)
     response = _dispatch(response_type)(content)
     return response
 
+
+# TODO: add joblib and ratelimit and  to requirements.txt
 if _USE_JOBLIB_CACHE:
     import joblib
+
     one_gb = 1000000000
-    location = '/tmp/joblib_cache'
+    location = "/tmp/joblib_cache"
     memory = joblib.Memory(location, verbose=1, bytes_limit=one_gb)
     if _THROTTLE_REQUESTS:
         from ratelimit import limits, sleep_and_retry
+
         period_seconds = 1
         calls_per_second = 20
-        _get_request = memory.cache(sleep_and_retry(limits(calls=calls_per_second, period=period_seconds)(_get_request)))
+        _get_request = memory.cache(
+            sleep_and_retry(
+                limits(calls=calls_per_second, period=period_seconds)(_get_request)
+            )
+        )
     else:
         _get_request = memory.cache(_get_request)
